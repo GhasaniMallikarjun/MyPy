c86612c14d19ee10c050041a9e917e5899849810
GhasaniMallikarjun <mallikarjunghasani18@gmail.com>
2025-07-09 23:41:53 +0530

    Py8_Browser

---
+++ Py8_Browser/main.py
@@ -0,0 +1,48 @@
+from PyQt5.QtWidgets  import QApplication, QMainWindow,  QToolBar, QAction, QlineEdit
+from PyQt5.QtCore import QUrl
+from PyQt5.QtWebEngineWidgets import import QWebEngineView
+import sys
+
+class BrowserWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.browser=QWebEngineView()
+        self.browser.setUrl(QUrl("https://duckduckgo.com/"))
+        self.setCentralWidget(self.browser)
+        self.showMaximized()
+
+
+        nav_bar=QToolBar()
+        self.addToolBar(nav_bar)
+
+        back_button=QAction("⮜", self)
+        back_button.triggered.connect(self.browser.back)
+        nav_bar.addAction(back_button)
+
+        forword_button=QAction("⮞", self)
+        forword_button.triggered.connect(self.browser.forward)
+        nav_bar.addAction(forword_button)
+
+
+        reload_button=QAction("⟳", self)
+        reload_button.triggered.connect(self.browser.reload)
+        nav_bar.addAction(reload_button)
+
+        self.url_bar=QLineEdit()
+        self.url_bar.returnPressed.connect(self.load_url)
+        nav_bar.addWidget(self.url_bar)
+
+        self.browser.urlChanged.connect(self.update_url_bar)
+
+    def load_url(self):
+        url=self.url_bar.text()
+        self.browser.setUrl(QUrl(url))
+
+    def update_url_bar(self, url):
+        self.url_bar.setText(url.toString())
+
+
+app=QApplication(sys.argv)
+app.setApplicationName("Simple Browser")
+window=BrowserWindow()
+app.exec_()
